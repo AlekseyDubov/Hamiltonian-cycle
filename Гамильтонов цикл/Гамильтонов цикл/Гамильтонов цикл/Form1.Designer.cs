1923d254a09251688c15d874957462fbcd25b79a
Jicei <31828588+Jicei@users.noreply.github.com>
2018-11-05 19:33:44 +0200

    Add files via upload

---
+++ Гамильтонов цикл/Гамильтонов цикл/Гамильтонов цикл/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Гамильтонов_цикл
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(728, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 59);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ручной ввод графа";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 24;
+            this.listBox2.Location = new System.Drawing.Point(558, 61);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(154, 340);
+            this.listBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(728, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(184, 58);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Пример графа";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 61);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(527, 340);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(209, 10);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 29);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Количество вершин:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(541, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 24);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Гамильтонов цикл:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(728, 168);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 58);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Выход";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 413);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Гамильтонов цикл/Гамильтонов цикл/Гамильтонов цикл/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Гамильтонов_цикл
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] c;   // номер шага, на котором посещается вершина
+        int[] path; // номера посещаемых вершин
+        int v0 = 2, n;//начальная вершина
+        int[,] a;//матрица смежности
+        void prnt()
+        {
+            int p;
+            for (p = 0; p < n; p++)
+                listBox2.Items.Add(path[p]);
+            listBox2.Items.Add(path[0]);
+        }
+        int gamilton(int k)//Функция гамильтонового цикл
+        {
+            int v, q1 = 0;
+            for (v = 0; v < n && !Convert.ToBoolean(q1); v++)
+            {
+                if (Convert.ToBoolean(a[v, path[k - 1]]) || Convert.ToBoolean(a[path[k - 1], v]))//Если вершины не равны 0, то есть соеденины
+                {
+                    //Номер элемента=кол-ву заданных вершин
+                    if (k == n && v == v0) //Выход из цикла
+                        q1 = 1;
+                    else 
+                        if (c[v] == -1)//не использованный ход
+                    {
+                        c[v] = k;//номер посещеного шага=номеру элемента
+                        path[k] = v;//Сохраняем номер посещенной вершины
+                        q1 = gamilton(k + 1);//рекурсивный вызов
+                        if (!Convert.ToBoolean(q1)) 
+                            c[v] = -1;//шаг не использван
+                    }
+                }
+            } return q1;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Проверка на гамильтонов цикл, по м
+            n = (int)(numericUpDown1.Value);//количество вершин
+            path = new int[n];
+            c = new int[n];
+            int i, j, p;
+            string elem;
+            a = new int[n, n];
+            dataGridView1.Rows.Clear();
+            listBox2.Items.Clear();
+            dataGridView1.RowCount = n + 1;//количество столбцов
+            dataGridView1.ColumnCount = n + 1;//кол-во строк
+            dataGridView1.Rows[0].Cells[0].Value = "Граф:";
+            dataGridView1.Columns[0].Width = 110;//ширина ячейки
+            //Разметка матрицы
+            for (i = 0; i < n; i++)
+            {
+                dataGridView1.Rows[i + 1].Cells[0].Value = ("Вершина:" + Convert.ToString(i));
+                dataGridView1.Columns[i + 1].Width = 110;
+            }
+            for (j = 0; j < n; j++)
+            {
+                dataGridView1.Rows[0].Cells[j + 1].Value = ("Вершина:" + Convert.ToString(j));
+            }
+            int l;
+            l = 1;
+           // Задание матрицы смежности. 1-вершины соеденины. 0-не соеденины.
+            for (i = 0; i < n; i++)
+            {
+                for (j = l; j < n; j++)
+                {
+                    elem = Microsoft.VisualBasic.Interaction.InputBox("Введите элемент.1-Вершины соеденинины.0-Вершины не соеденины" + (i) + ":" + (j));
+                    while (Convert.ToInt32(elem) != 0 && Convert.ToInt32(elem) != 1)
+                    {
+                        MessageBox.Show("Не правильно введены данные.Введите их еще раз");
+                        elem = Microsoft.VisualBasic.Interaction.InputBox("Введите элемент.1-Вершины соеденинины.0-Вершины не соеденины" + (i) + ":" + (j));
+                    }
+                    if (elem.Length > 0)//для возможности отмены
+                    {
+                        while (true)
+                        {
+                            try
+                            {
+                                a[i,j] = Convert.ToInt32(elem);//задание элемента матрицы
+                                break;
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Не правильно введены данные.");
+                                elem = Microsoft.VisualBasic.Interaction.InputBox("Введите элемент.1-Вершины соеденинины.0-Вершины не соеденины " + (i) + ":" + (j));
+                                if (elem.Length <= 0)
+                                {
+                                    return;
+                                }
+                                while (Convert.ToInt32(elem) != 0 && Convert.ToInt32(elem) != 1)//Проверка на 0 и 1
+                                {
+                                    MessageBox.Show("Не правильно введены данные.Введите их еще раз");
+                                    elem = Microsoft.VisualBasic.Interaction.InputBox("Введите элемент.1-Вершины соеденинины.0-Вершины не соеденины" + (i) + ":" + (j));
+                                }
+                            }
+                        }
+                    }
+                    dataGridView1.Rows[i+1].Cells[j+1].Value = a[i, j];//Добавление элемента в дата гридвью
+                    a[j, i] = a[i, j];//матрица смежности-семетрична
+                    dataGridView1.Rows[j + 1].Cells[i + 1].Value = a[j, i];
+                }
+                l++;
+            }
+            //Заполняем главную диагональ нулями. Вершина не может быть соеденина сама с собой.
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    if(i==j)
+                        dataGridView1.Rows[i + 1].Cells[j + 1].Value = 0;
+                }
+            }
+            //Сам гамильтонов цикл
+            for (j = 0; j < n; j++) 
+                c[j] = -1;//номер хода -1, расчет номера хода начинается с 0
+            path[0] = v0;//Первая посещенная вершина 2
+            c[v0] = v0;//2 шаг на котором посещается вершина.
+            //Выводим посещенные вершины
+            if (Convert.ToBoolean(gamilton(1)))
+            {
+                for (p = 0; p < n; p++)
+                    listBox2.Items.Add(path[p]);
+                listBox2.Items.Add(path[0]);
+            }
+            else
+                MessageBox.Show("Данный граф не гамильтонов.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            n = 10;//количество вершин
+            path = new int[n];
+            c = new int[n];
+            int i, j, p;
+            a = new int[,]//Предварительное задание графа(матрицы смежности)
+            {
+                {0,0,0,0,0,1,0,0,0,0},
+                {0,0,1,0,0,0,1,0,0,0},
+                {0,1,0,1,0,0,0,1,0,0},
+                {0,0,1,0,1,0,0,0,1,0},
+                {1,0,0,1,0,0,0,0,0,1},
+                {0,0,0,0,0,0,1,0,0,1},
+                {0,0,0,1,0,0,0,1,0,0},
+                {0,0,0,0,1,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0,0,1},
+                {0,0,0,0,0,0,0,0,0,0}
+            };
+            //Очищение
+            dataGridView1.Rows.Clear();
+            listBox2.Items.Clear();
+            //Задание столбцов и строк в графическом элементе.
+            dataGridView1.RowCount = n + 1;
+            dataGridView1.ColumnCount = n + 1;
+            dataGridView1.Rows[0].Cells[0].Value = "Граф:";//0 ячейка-граф
+            dataGridView1.Columns[0].Width = 110;//ширина ячейки
+            for (i = 0; i < n; i++)
+            {
+                dataGridView1.Rows[i + 1].Cells[0].Value = ("Вершина:" + Convert.ToString(i));
+                dataGridView1.Columns[i + 1].Width = 110;
+            }
+            //Разметка вершин
+            for (j = 0; j < n; j++)
+            {
+                dataGridView1.Rows[0].Cells[j + 1].Value = ("Вершина:" + Convert.ToString(j));
+            }
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    dataGridView1.Rows[i + 1].Cells[j + 1].Value = a[i, j];
+                }
+            }
+            //Ufvbkmnjyjd wbrk
+            for (j = 0; j < n; j++)
+                c[j] = -1;
+            path[0] = v0;
+            c[v0] = v0;
+            if (Convert.ToBoolean(gamilton(1)))
+            {
+                for (p = 0; p < n; p++)
+                    listBox2.Items.Add(path[p]);
+                listBox2.Items.Add(path[0]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
